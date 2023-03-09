d30b36e03d00c80dd87e0bcb9ac2db4e8a68f9a5
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-03-09 17:28:28 -0300

    Feat: implementa entidade Torcedor

--- src/main/java/com/trybe/gestaotime/model/Torcedor.java
+++ src/main/java/com/trybe/gestaotime/model/Torcedor.java
@@ -1,17 +1,50 @@
 package com.trybe.gestaotime.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 /**
  * Classe Torcedor.
  **/
 
+@Entity
 public class Torcedor {
 
-  /**
-   * Atributos.
-   **/
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  /**
-   * Métodos.
-   **/
+  private String nome;
 
+  @ManyToMany(mappedBy = "torcedores")
+  private List<Time> times;
+
+  public Torcedor() {
+    this.times = new ArrayList<Time>();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Time> getTimes() {
+    return times;
+  }
+
+  public void setTimes(Time time) {
+    this.times.add(time);
+  }
 }
